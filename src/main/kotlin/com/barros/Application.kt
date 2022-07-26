012cf1ab90028bb2f18084f2092459d25acf02ab
Barros9 <giu.barreca@gmail.com>
2022-07-26 10:13:34 +0200

    Using only Application

--- src/main/kotlin/com/barros/Application.kt
+++ src/main/kotlin/com/barros/Application.kt
@@ -1,11 +1,25 @@
 package com.barros
 
-import com.barros.plugins.configureRouting
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
         configureRouting()
     }.start(wait = true)
+}
+
+fun Application.configureRouting() {
+
+    // Starting point for a Ktor app:
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+    routing {
+    }
 }
