97190e4399f6e9371b81e2ebf7ce6164adfa0477
Barros9 <giu.barreca@gmail.com>
2022-07-26 10:46:25 +0200

    Added Movie

---
+++ src/main/kotlin/com/barros/Movie.kt
@@ -0,0 +1,14 @@
+package com.barros
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Movie(
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val posterPath: String,
+    val releaseDate: String,
+    val voteAverage: Double,
+    val popularity: Double
+)
