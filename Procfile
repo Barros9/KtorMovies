3182e0ed2bad9d2bef6ab735f56fd37a6b9d2db5
Barros9 <giu.barreca@gmail.com>
2022-07-27 11:33:28 +0200

    Prepare app for deploying

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/ktor-movies/bin/ktor-movies
--- src/main/kotlin/com/barros/Application.kt
+++ src/main/kotlin/com/barros/Application.kt
@@ -10,7 +10,7 @@ import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
+    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
         install(ContentNegotiation) {
             json(Json {
                 prettyPrint = true
